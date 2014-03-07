628f9769e1cd125ca944fc15a99b0ee779d6369a
Holden Karau <holden@pigscanfly.ca>
2014-03-06 16:02:51 -0800

    Fix basicmap indentation

--- src/main/java/com/oreilly/learningsparkexamples/java/BasicMap.java
+++ src/main/java/com/oreilly/learningsparkexamples/java/BasicMap.java
@@ -22,9 +22,9 @@ public class BasicMap {
 		}
 		JavaSparkContext sc = new JavaSparkContext(
       master, "basicmap", System.getenv("SPARK_HOME"), System.getenv("JARS"));
-    JavaRDD<Integer> rdd = sc.parallelize(Arrays.asList(1,2,3, 4));
+    JavaRDD<Integer> rdd = sc.parallelize(Arrays.asList(1, 2, 3, 4));
     JavaRDD<Integer> result = rdd.map(
-        new Function<Integer, Integer>() { public Integer call(Integer x) { return x*x;}});
+      new Function<Integer, Integer>() { public Integer call(Integer x) { return x*x;}});
     System.out.println(StringUtils.join(result.collect(), ","));
 	}
 }
