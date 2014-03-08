1d3719bcd03b92d04efae10933928f953d95c7a4
Holden Karau <holden@pigscanfly.ca>
2014-03-08 15:41:34 -0800

    Add a simple basicmap python example

---
+++ src/python/BasicMap.py
@@ -0,0 +1,26 @@
+"""
+>>> from pyspark.context import SparkContext
+>>> sc = SparkContext('local', 'test')
+>>> b = sc.parallelize([1, 2, 3, 4])
+>>> sorted(basicSquare(b).collect())
+[1, 4, 9, 12]
+"""
+
+import sys
+
+from pyspark import SparkContext
+
+def basicSquare(nums):
+    """Square the numbers"""
+    return nums.map(lambda x: x * x)
+
+if __name__ == "__main__":
+    master = "local"
+    if len(sys.argv) = 2:
+        master = sys.argv[1]
+    sc = SparkContext(master, "BasicMap")
+    nums = sc.parallelize([1, 2, 3, 4])
+    output = countWords(nums)
+    for num in output:
+        print "%i " % (num)
+
