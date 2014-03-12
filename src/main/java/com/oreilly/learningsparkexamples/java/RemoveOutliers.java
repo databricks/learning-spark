40b801d3a4a9ed5735027e84620b9321a925d1b9
Holden Karau <holden@pigscanfly.ca>
2014-03-12 16:38:50 -0700

    Add removeoutliers for java and basicavg for scala and java

---
+++ src/main/java/com/oreilly/learningsparkexamples/java/RemoveOutliers.java
@@ -0,0 +1,38 @@
+/**
+ * Illustrates remove outliers in Java using summary Stats
+ */
+package com.oreilly.learningsparkexamples.java;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaDoubleRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.util.StatCounter;
+
+public class RemoveOutliers {
+  public static void main(String[] args) {
+		String master;
+		if (args.length > 0) {
+      master = args[0];
+		} else {
+			master = "local";
+		}
+		JavaSparkContext sc = new JavaSparkContext(
+      master, "basicmap", System.getenv("SPARK_HOME"), System.getenv("JARS"));
+    JavaDoubleRDD input = sc.parallelizeDoubles(Arrays.asList(1.0, 2.0, 3.0, 4.0));
+    JavaDoubleRDD result = removeOutliers(input);
+    System.out.println(StringUtils.join(result.collect(), ","));
+  }
+  static JavaDoubleRDD removeOutliers(JavaDoubleRDD rdd) {
+    final StatCounter summaryStats = rdd.stats();
+    final Double stddev = Math.sqrt(summaryStats.variance());
+    return rdd.filter(new Function<Double, Boolean>() { public Boolean call(Double x) {
+          return (Math.abs(x-summaryStats.mean()) < 3 * stddev);
+        }});
+  }
+}
