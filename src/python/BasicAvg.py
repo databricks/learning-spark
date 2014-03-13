56319a8562d10b43189ffa968b5ecc477f50a6c9
Holden Karau <holden@pigscanfly.ca>
2014-03-13 12:40:26 -0700

    Add a basic avg example

---
+++ src/python/BasicAvg.py
@@ -0,0 +1,26 @@
+"""
+>>> from pyspark.context import SparkContext
+>>> sc = SparkContext('local', 'test')
+>>> b = sc.parallelize([1, 2, 3, 4])
+>>> basicAvg(b)
+2.5
+"""
+
+import sys
+
+from pyspark import SparkContext
+
+def basicAvg(nums):
+    """Compute the avg"""
+    sumCount = nums.map(lambda x: (x,1)).fold((0, 0), (lambda x, y: (x[0] + y[0], x[1] + y[1])))
+    return sumCount[0] / float(sumCount[1])
+
+if __name__ == "__main__":
+    master = "local"
+    if len(sys.argv) == 2:
+        master = sys.argv[1]
+    sc = SparkContext(master, "Sum")
+    nums = sc.parallelize([1, 2, 3, 4])
+    avg = basicAvg(nums)
+    print avg
+
