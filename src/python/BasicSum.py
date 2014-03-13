7396849e46748bc3aed537a0d3678da0977cc77e
Holden Karau <holden@pigscanfly.ca>
2014-03-13 12:17:25 -0700

    Add BasicSum python example

---
+++ src/python/BasicSum.py
@@ -0,0 +1,25 @@
+"""
+>>> from pyspark.context import SparkContext
+>>> sc = SparkContext('local', 'test')
+>>> b = sc.parallelize([1, 2, 3, 4])
+>>> sorted(basicSquareNoOnes(b).collect())
+[4, 9, 16]
+"""
+
+import sys
+
+from pyspark import SparkContext
+
+def basicSum(nums):
+    """Sum the numbers"""
+    return nums.fold(0, (lambda x, y: x + y))
+
+if __name__ == "__main__":
+    master = "local"
+    if len(sys.argv) == 2:
+        master = sys.argv[1]
+    sc = SparkContext(master, "Sum")
+    nums = sc.parallelize([1, 2, 3, 4])
+    output = basicSum(nums)
+    print output
+
