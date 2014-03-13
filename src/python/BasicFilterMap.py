08ceb134df4378ab86acae27cb23f4ab3b215dec
Holden Karau <holden@pigscanfly.ca>
2014-03-13 11:47:37 -0700

    More python examples

--- src/python/BasicFilterMap.py
+++ src/python/BasicFilterMap.py
@@ -2,25 +2,25 @@
 >>> from pyspark.context import SparkContext
 >>> sc = SparkContext('local', 'test')
 >>> b = sc.parallelize([1, 2, 3, 4])
->>> sorted(basicSquare(b).collect())
-[1, 4, 9, 16]
+>>> sorted(basicSquareNoOnes(b).collect())
+[4, 9, 16]
 """
 
 import sys
 
 from pyspark import SparkContext
 
-def basicSquare(nums):
+def basicSquareNoOnes(nums):
     """Square the numbers"""
-    return nums.map(lambda x: x * x)
+    return nums.map(lambda x: x * x).filter(lambda x: x != 1)
 
 if __name__ == "__main__":
     master = "local"
     if len(sys.argv) == 2:
         master = sys.argv[1]
-    sc = SparkContext(master, "BasicMap")
+    sc = SparkContext(master, "BasicFilterMap")
     nums = sc.parallelize([1, 2, 3, 4])
-    output = sorted(basicSquare(nums).collect())
+    output = sorted(basicSquareNoOnes(nums).collect())
     for num in output:
         print "%i " % (num)
 
