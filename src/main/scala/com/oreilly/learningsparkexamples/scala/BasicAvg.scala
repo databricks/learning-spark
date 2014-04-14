8da5b00ef6c32dfe0260492d56d3d71f96fff2c1
Holden Karau <holden@pigscanfly.ca>
2014-04-14 11:37:20 -0700

    update name of basicavg in scala code

--- src/main/scala/com/oreilly/learningsparkexamples/scala/BasicAvg.scala
+++ src/main/scala/com/oreilly/learningsparkexamples/scala/BasicAvg.scala
@@ -5,13 +5,13 @@ package com.oreilly.learningsparkexamples.scala
 
 import org.apache.spark._
 
-object BasicAverage {
+object BasicAvg {
     def main(args: Array[String]) {
       val master = args.length match {
         case x: Int if x > 0 => args(0)
         case _ => "local"
       }
-      val sc = new SparkContext(master, "BasicMap", System.getenv("SPARK_HOME"))
+      val sc = new SparkContext(master, "BasicAvg", System.getenv("SPARK_HOME"))
       val input = sc.parallelize(List(1,2,3,4))
       val result = input.aggregate((0, 0))((x, y) => (x._1 + y, x._2 + 1),
         (x,y) => (x._1 + y._1, x._2 + y._2))
