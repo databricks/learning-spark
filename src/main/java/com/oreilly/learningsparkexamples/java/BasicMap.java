8d7402a25ea732faeb73a7c719cc89ef7f28e0ad
Holden Karau <holden@pigscanfly.ca>
2014-04-14 10:27:53 -0700

    Fix random indentation in BasicMap

--- src/main/java/com/oreilly/learningsparkexamples/java/BasicMap.java
+++ src/main/java/com/oreilly/learningsparkexamples/java/BasicMap.java
@@ -14,17 +14,17 @@ import org.apache.spark.api.java.function.Function;
 
 public class BasicMap {
   public static void main(String[] args) throws Exception {
-		String master;
-		if (args.length > 0) {
+    String master;
+    if (args.length > 0) {
       master = args[0];
-		} else {
-			master = "local";
-		}
-		JavaSparkContext sc = new JavaSparkContext(
+    } else {
+      master = "local";
+    }
+    JavaSparkContext sc = new JavaSparkContext(
       master, "basicmap", System.getenv("SPARK_HOME"), System.getenv("JARS"));
     JavaRDD<Integer> rdd = sc.parallelize(Arrays.asList(1, 2, 3, 4));
     JavaRDD<Integer> result = rdd.map(
       new Function<Integer, Integer>() { public Integer call(Integer x) { return x*x;}});
     System.out.println(StringUtils.join(result.collect(), ","));
-	}
+  }
 }
