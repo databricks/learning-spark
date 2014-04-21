1dcb41ba6444665a661fa425f07f3c1d2882d22f
Holden Karau <holden@pigscanfly.ca>
2014-04-21 12:23:11 -0700

    Add a basic map partitions examples for python

---
+++ src/python/BasicMapPartitions.py
@@ -0,0 +1,33 @@
+"""
+>>> from pyspark.context import SparkContext
+>>> sc = SparkContext('local', 'test')
+>>> b = sc.parallelize(["KK6JKQ", "Ve3UoW", "kk6jlk", "W6BB"])
+>>> fetchCallSigns(b).size()
+4
+"""
+
+import sys
+import urllib3
+
+from pyspark import SparkContext
+
+def processCallSigns(signs):
+    """Process call signs"""
+    http = urllib3.PoolManager()
+    requests = map(lambda x : http.request('GET', "http://qrzcq.com/call/" + x), signs)
+    return map(lambda x : x.data, requests)
+
+def fetchCallSigns(input):
+    """Fetch call signs"""
+    return input.mapPartitions(lambda callSigns : processCallSigns(callSigns))
+
+if __name__ == "__main__":
+    master = "local"
+    if len(sys.argv) == 2:
+        master = sys.argv[1]
+    sc = SparkContext(master, "BasicMapPartitions")
+    input = sc.parallelize(["KK6JKQ", "Ve3UoW", "kk6jlk", "W6BB"])
+    output = sorted(fetchCallSigns(input).collect())
+    for str in output:
+        print "%s " % (str)
+
