6894f6b1824faaa90899e39720f107af4ac7bd33
Holden Karau <holden@pigscanfly.ca>
2014-06-08 16:34:58 -0700

    Make the basicflatmap example be a wordcount example

--- src/main/java/com/oreilly/learningsparkexamples/java/BasicFlatMap.java
+++ src/main/java/com/oreilly/learningsparkexamples/java/BasicFlatMap.java
@@ -5,10 +5,14 @@ package com.oreilly.learningsparkexamples.java;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
 
 import org.apache.commons.lang.StringUtils;
 
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.FlatMapFunction;
 
@@ -22,10 +26,13 @@ public class BasicFlatMap {
     JavaSparkContext sc = new JavaSparkContext(
       args[0], "basicflatmap", System.getenv("SPARK_HOME"), System.getenv("JARS"));
     JavaRDD<String> rdd = sc.textFile(args[1]);
-    JavaRDD<String> result = rdd.flatMap(
+    JavaRDD<String> words = rdd.flatMap(
       new FlatMapFunction<String, String>() { public Iterable<String> call(String x) {
           return Arrays.asList(x.split(" "));
         }});
-    System.out.println(StringUtils.join(result.collect(), ","));
+    Map<String, Long> result = words.countByValue();
+    for (Entry<String, Long> entry: result.entrySet()) {
+      System.out.println(entry.getKey() + ":" + entry.getValue());
+    }
   }
 }
