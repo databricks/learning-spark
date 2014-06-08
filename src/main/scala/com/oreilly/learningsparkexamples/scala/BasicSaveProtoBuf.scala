11aab208645299ce0a7ff90953cd7478fe9055fe
Holden Karau <holden@pigscanfly.ca>
2014-06-08 16:23:41 -0700

    Fix up saveprotobuf example

--- src/main/scala/com/oreilly/learningsparkexamples/scala/BasicSaveProtoBuf.scala
+++ src/main/scala/com/oreilly/learningsparkexamples/scala/BasicSaveProtoBuf.scala
@@ -8,15 +8,30 @@ import com.oreilly.learningsparkexamples.proto.Places
 import org.apache.spark._
 import org.apache.spark.SparkContext._
 
+import org.apache.hadoop.io.Text
+import com.twitter.elephantbird.mapreduce.io.ProtobufWritable
+import com.twitter.elephantbird.mapreduce.output.LzoProtobufBlockOutputFormat
+import org.apache.hadoop.mapreduce.Job
+
 object BasicSaveProtoBuf {
     def main(args: Array[String]) {
       val master = args(0)
       val outputFile = args(1)
       val sc = new SparkContext(master, "BasicSaveProtoBuf", System.getenv("SPARK_HOME"))
+      val job = new Job()
+      val conf = job.getConfiguration
+      LzoProtobufBlockOutputFormat.setClassConf(classOf[Places.Venue], conf);
       val dnaLounge = Places.Venue.newBuilder()
       dnaLounge.setId(1);
       dnaLounge.setName("DNA Lounge")
       dnaLounge.setType(Places.Venue.VenueType.CLUB)
       val data = sc.parallelize(List(dnaLounge.build()))
+      val outputData = data.map{ pb =>
+        val protoWritable = ProtobufWritable.newInstance(classOf[Places.Venue]);
+        protoWritable.set(pb)
+        (null, protoWritable)
+      }
+      outputData.saveAsNewAPIHadoopFile(outputFile, classOf[Text], classOf[ProtobufWritable[Places.Venue]],
+        classOf[LzoProtobufBlockOutputFormat[ProtobufWritable[Places.Venue]]], conf)
     }
 }
