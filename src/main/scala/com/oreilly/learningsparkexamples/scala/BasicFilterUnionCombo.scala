916cac7ae7190b588da8bf0442d0f06b7f19f78c
Holden Karau <holden@pigscanfly.ca>
2014-06-20 14:31:00 -0700

    Add an example using a source rdd filtered in two different ways and then unioned together to pull out the lines with error or warning

---
+++ src/main/scala/com/oreilly/learningsparkexamples/scala/BasicFilterUnionCombo.scala
@@ -0,0 +1,20 @@
+/**
+ * Illustrates filtering and union to extract lines with "error" or "warning"
+ */
+package com.oreilly.learningsparkexamples.scala
+
+import org.apache.spark._
+import org.apache.spark.SparkContext._
+
+object BasicFilterUnionCombo {
+    def main(args: Array[String]) {
+      val conf = new SparkConf
+      conf.setMaster(args(0))
+      val sc = new SparkContext(conf)
+      val inputRDD = sc.textFile(args(1))
+      val errorsRDD = inputRDD.filter(_.contains("error"))
+      val warningsRDD = inputRDD.filter(_.contains("error"))
+      val badLinesRDD = errorsRDD.union(warningsRDD)
+      println(badLinesRDD.collect().mkString("\n"))
+    }
+}
