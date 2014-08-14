ad3db919eb0bcc334fb3cd7ebf517132394704ab
Holden Karau <holden@pigscanfly.ca>
2014-08-14 14:23:34 -0700

    Move QSO to a seperate file so Jackson is happy pandas

---
+++ src/main/java/com/oreilly/learningsparkexamples/java/QSO.java
@@ -0,0 +1,11 @@
+package com.oreilly.learningsparkexamples.java;
+
+import java.io.Serializable;
+
+public class QSO implements Serializable {
+  public String callsign;
+  public Double contactlat;
+  public Double contactlong;
+  public Double mylat;
+  public Double mylong;
+}
