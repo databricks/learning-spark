7f7d1c5336107ec25f2bbd2b000597a011c98d11
Holden Karau <holden@pigscanfly.ca>
2014-09-04 15:02:25 -0700

    stop reading separator from env

--- src/R/finddistance.R
+++ src/R/finddistance.R
@@ -2,10 +2,9 @@
 library("Imap")
 f <- file("stdin")
 open(f)
-separator <- Sys.getenv(c("SEPARATOR"))
 while(length(line <- readLines(f,n=1)) > 0) {
   # process line
-  contents <- Map(as.numeric, strsplit(line, separator))
+  contents <- Map(as.numeric, strsplit(line, ","))
   mydist <- gdist(contents[[1]][1], contents[[1]][2], contents[[1]][3], contents[[1]][4],
                   units="m", a=6378137.0, b=6356752.3142, verbose = FALSE)
   output = paste(mydist, collapse=" ")
