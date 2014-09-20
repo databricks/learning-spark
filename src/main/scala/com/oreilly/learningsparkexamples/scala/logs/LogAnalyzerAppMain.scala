861a1dd1c4978ca7c98627f99a9ea84263eb8839
Holden Karau <holden@pigscanfly.ca>
2014-09-19 18:06:33 -0700

    Start adding the scala example

---
+++ src/main/scala/com/oreilly/learningsparkexamples/scala/logs/LogAnalyzerAppMain.scala
@@ -0,0 +1,33 @@
+package com.oreilly.learningsparkexamples.scala.logs;
+
+import org.apahce.spark._
+import org.apache.spark.streaming._
+
+/**
+ * The LogAnalyzerAppMain is an sample logs analysis application.  For now,
+ * it is a simple minimal viable product:
+ *   - Read in new log files from a directory and input those new files into streaming.
+ *   - Computes stats for all of time as well as the last time interval based on those logs.
+ *   - Write the calculated stats to an txt file on the local file system
+ *     that gets refreshed every time interval.
+ *
+ * Once you get this program up and running, feed apache access log files
+ * into the local directory of your choosing.
+ *
+ * Then open your output text file, perhaps in a web browser, and refresh
+ * that page to see more stats come in.
+ *
+ * Modify the command line flags to the values of your choosing.
+ * Notice how they come after you specify the jar when using spark-submit.
+ *
+ * Example command to run:
+ * %  ${YOUR_SPARK_HOME}/bin/spark-submit
+ *     --class "com.oreilly.learningsparkexamples.java.logs.LogAnalyzerAppMain"
+ *     --master local[4]
+ *     target/uber-log-analyzer-1.0.jar
+ *     --logs_directory /tmp/logs
+ *     --output_html_file /tmp/log_stats.html
+ *     --index_html_template ./src/main/resources/index.html.template
+ */
+object LogAnalyzerAppMain {
+}
