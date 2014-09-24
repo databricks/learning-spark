f5bd9e7c20d033d5c512b5158a064baff9b88500
Holden Karau <holden@pigscanfly.ca>
2014-09-24 14:22:04 -0700

    Add simple streaming log input example

---
+++ src/main/java/com/oreilly/learningsparkexamples/java/StreamingLogInput.java
@@ -0,0 +1,35 @@
+/**
+ * Illustrates a simple map then filter in Java
+ */
+package com.oreilly.learningsparkexamples.java;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.Duration;
+
+public class StreamingLogInput {
+  public static void main(String[] args) throws Exception {
+		String master = args[0];
+		JavaSparkContext sc = new JavaSparkContext(
+      master, "StreamingLogInput", System.getenv("SPARK_HOME"), System.getenv("JARS"));
+    // Create a StreamingContext with a 1 second batch size
+    JavaStreamingContext jssc = new JavaStreamingContext(sc, new Duration(1000));
+    // Create a DStream from all the input on port 7777
+    JavaDStream<String> lines = jssc.socketTextStream("localhost", 7777);
+    // Filter our DStream for lines with "error"
+    JavaDStream<String> errorLines = lines.filter(new Function<String, Boolean>() {
+        public Boolean call(String line) {
+          return line.contains("error");
+        }});
+    // Print out the lines with errors, which causes this DStream to be evaluated
+    errorLines.print();
+	}
+}
