ba02a3ac4607be63f4bbc2ab3ca2999c9e9a3068
Holden Karau <holden@pigscanfly.ca>
2014-09-30 18:59:58 -0700

    basic kafka example

--- build.sbt
+++ build.sbt
@@ -19,6 +19,7 @@ libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-sql" % "1.1.0" % "provided",
   "org.apache.spark" %% "spark-hive" % "1.1.0" % "provided",
   "org.apache.spark" %% "spark-streaming" % "1.1.0" % "provided",
+  "org.apache.spark" %% "spark-streaming-kafka" % "1.1.0" % "provided",
   "org.apache.commons" % "commons-lang3" % "3.0",
   "org.eclipse.jetty"  % "jetty-client" % "8.1.14.v20131031",
   "com.typesafe.play" % "play-json_2.10" % "2.2.1",
--- src/main/scala/com/oreilly/learningsparkexamples/scala/StreamingLogInput.scala
+++ src/main/scala/com/oreilly/learningsparkexamples/scala/StreamingLogInput.scala
@@ -6,6 +6,7 @@ package com.oreilly.learningsparkexamples.scala
 import org.apache.spark._
 import org.apache.spark.SparkContext._
 import org.apache.spark.streaming._
+import org.apache.spark.streaming.dstream._
 
 object StreamingLogInput {
   def main(args: Array[String]) {
@@ -15,14 +16,17 @@ object StreamingLogInput {
     val ssc = new StreamingContext(sc, Seconds(1))
     // Create a DStream from all the input on port 7777
     val lines = ssc.socketTextStream("localhost", 7777)
-    // Filter our DStream for lines with "error"
-    val errorLines = lines.filter(_.contains("error"))
-    // Print out the lines with errors, which causes this DStream to be evaluated
-    errorLines.print()
+    processLines(lines)
     // start our streaming context and wait for it to "finish"
     ssc.start()
     // Wait for 10 seconds then exit. To run forever call without a timeout
     ssc.awaitTermination(10000)
     ssc.stop()
+  }
+  def processLines(lines: DStream[String]) {
+    // Filter our DStream for lines with "error"
+    val errorLines = lines.filter(_.contains("error"))
+    // Print out the lines with errors, which causes this DStream to be evaluated
+    errorLines.print()
   }
 }
