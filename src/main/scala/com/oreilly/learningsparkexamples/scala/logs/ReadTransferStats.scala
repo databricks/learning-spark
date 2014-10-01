984bd4fe9484d02e53357414422d0aab8529d200
Holden Karau <holden@pigscanfly.ca>
2014-09-30 18:20:43 -0700

    Add a ReadTransferStats example in Scala

--- src/main/scala/com/oreilly/learningsparkexamples/scala/logs/ReadTransferStats.scala
+++ src/main/scala/com/oreilly/learningsparkexamples/scala/logs/ReadTransferStats.scala
@@ -6,6 +6,7 @@ package com.oreilly.learningsparkexamples.scala.logs
 import org.apache.spark._
 import org.apache.spark.SparkContext._
 import org.apache.spark.streaming._
+import org.apache.spark.streaming.dstream._
 
 import org.apache.hadoop.io.Writable
 import org.apache.hadoop.io.IntWritable
@@ -14,8 +15,11 @@ import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat
 
 
 object ReadTransferStats {
-  def readStats(ssc: StreamingContext, inputDirectory: String) = {
+  def readStats(ssc: StreamingContext, inputDirectory: String): DStream[(Long, Int)] = {
+    // convert the input from Writables to native types
     ssc.fileStream[LongWritable, IntWritable,
-      SequenceFileInputFormat[LongWritable, IntWritable]](inputDirectory)
+      SequenceFileInputFormat[LongWritable, IntWritable]](inputDirectory).map{
+      case (x, y) => (x.get(), y.get())
+    }
   }
 }
