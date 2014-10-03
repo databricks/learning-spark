60935202969bcddcd73883c5beb55d243cf87139
Holden Karau <holden@pigscanfly.ca>
2014-10-03 14:29:05 -0700

    print the results

--- src/main/scala/com/oreilly/learningsparkexamples/scala/FlumeInput.scala
+++ src/main/scala/com/oreilly/learningsparkexamples/scala/FlumeInput.scala
@@ -19,7 +19,7 @@ object FlumeInput {
     val events = FlumeUtils.createStream(ssc, receiverHostname, receiverPort)
     // Assuming that our flume events are UTF-8 log lines
     val lines = events.map{e => new String(e.event.getBody().array(), "UTF-8")}
-    StreamingLogInput.processLines(lines)
+    lines.print()
     // start our streaming context and wait for it to "finish"
     ssc.start()
     // Wait for 10 seconds then exit. To run forever call without a timeout
