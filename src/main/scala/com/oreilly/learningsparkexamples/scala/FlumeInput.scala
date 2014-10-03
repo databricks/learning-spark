3eba8d2c75d1b82fa3cc9e7eea6cecaf47d0143f
Holden Karau <holden@pigscanfly.ca>
2014-10-03 14:30:28 -0700

    more chatty flume input

--- src/main/scala/com/oreilly/learningsparkexamples/scala/FlumeInput.scala
+++ src/main/scala/com/oreilly/learningsparkexamples/scala/FlumeInput.scala
@@ -16,14 +16,17 @@ object FlumeInput {
     val sc = new SparkContext(conf)
     // Create a StreamingContext with a 1 second batch size
     val ssc = new StreamingContext(sc, Seconds(1))
+    println(s"Creating flume stream on $receiverHostname $receiverPort")
     val events = FlumeUtils.createStream(ssc, receiverHostname, receiverPort)
     // Assuming that our flume events are UTF-8 log lines
     val lines = events.map{e => new String(e.event.getBody().array(), "UTF-8")}
+    println("Starting StreamingContext")
     lines.print()
     // start our streaming context and wait for it to "finish"
     ssc.start()
     // Wait for 10 seconds then exit. To run forever call without a timeout
     ssc.awaitTermination(10000)
     ssc.stop()
+    println("Done")
   }
 }
