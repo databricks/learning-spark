d838e9fdfae48b1746c5986fba456f62cb7020a8
Holden Karau <holden@pigscanfly.ca>
2014-10-10 17:07:24 -0700

    Add a LoadJsonWithSparkSQL example in Scala

---
+++ src/main/scala/com/oreilly/learningsparkexamples/scala/LoadJsonWithSparkSQL.scala
@@ -0,0 +1,23 @@
+/**
+ * Illustrates loading JSON data using Spark SQL
+ */
+package com.oreilly.learningsparkexamples.scala
+
+import org.apache.spark._
+import org.apache.spark.sql.SQLContext
+
+
+object LoadJsonWithSparkSQL {
+  def main(args: Array[String]) {
+    if (args.length != 2) {
+      println("Usage: [sparkmaster] [inputFile]")
+      exit(1)
+    }
+    val master = args(0)
+    val inputFile = args(1)
+    val sc = new SparkContext(master, "LoadJsonWithSparkSQL", System.getenv("SPARK_HOME"))
+    val sqlCtx = new org.apache.spark.sql.SQLContext(sc)
+    val input = sqlCtx.jsonFile(inputFile)
+    input.printSchema()
+  }
+}
