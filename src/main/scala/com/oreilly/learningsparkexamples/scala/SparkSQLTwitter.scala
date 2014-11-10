868c79b274ea9ec4205e87cb7f9f1e3bec4f73ef
Holden Karau <holden@pigscanfly.ca>
2014-11-09 20:49:07 -0800

    set codegen on

--- src/main/scala/com/oreilly/learningsparkexamples/scala/SparkSQLTwitter.scala
+++ src/main/scala/com/oreilly/learningsparkexamples/scala/SparkSQLTwitter.scala
@@ -17,6 +17,7 @@ object SparkSQLTwitter {
       val inputFile = args(0)
       val outputFile = args(1)
       val conf = new SparkConf()
+      conf.set("spark.sql.codegen", "true")
       val sc = new SparkContext(conf)
       val sqlCtx = new SQLContext(sc)
       import sqlCtx._
