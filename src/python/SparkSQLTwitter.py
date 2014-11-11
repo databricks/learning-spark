f7ebf0d43a7644e9ec1d1aa75772c5fba4719eaf
Holden Karau <holden@pigscanfly.ca>
2014-11-10 17:46:29 -0800

    more hive. all the hive. omnomnom hive

--- src/python/SparkSQLTwitter.py
+++ src/python/SparkSQLTwitter.py
@@ -1,6 +1,6 @@
 # A simple demo for working with SparkSQL and Tweets
 from pyspark import SparkContext, SparkConf
-from pyspark.sql import SQLContext, Row
+from pyspark.sql import HiveContext, Row
 import json
 import sys
 
@@ -8,20 +8,20 @@ if __name__ == "__main__":
     inputFile = sys.argv[1]
     conf = SparkConf().setAppName("SparkSQLTwitter")
     sc = SparkContext()
-    sqlCtx = SQLContext(sc)
+    hiveCtx = HiveContext(sc)
     print "Loading tweets from " + inputFile
-    input = sqlCtx.jsonFile(inputFile)
+    input = hiveCtx.jsonFile(inputFile)
     input.registerTempTable("tweets")
-    topTweets = sqlCtx.sql("SELECT text, retweetCount FROM tweets ORDER BY retweetCount LIMIT 10")
+    topTweets = hiveCtx.hql("SELECT text, retweetCount FROM tweets ORDER BY retweetCount LIMIT 10")
     print topTweets.collect()
     topTweetText = topTweets.map(lambda row : row.text)
     print topTweetText.collect()
     # Make a happy person row
     happyPeopleRDD = sc.parallelize([Row(name="holden", favouriteBeverage="coffee")])
-    happyPeopleSchemaRDD = sqlCtx.inferSchema(happyPeopleRDD)
+    happyPeopleSchemaRDD = hiveCtx.inferSchema(happyPeopleRDD)
     happyPeopleSchemaRDD.registerTempTable("happy_people")
     # Make a UDF to tell us how long some text is
-    sqlCtx.registerFunction("strLenPython", lambda x: len(x), IntegerType())
-    lengthSchemaRDD = sqlCtx.sql("SELECT strLenPython('text') FROM tweets LIMIT 10")
+    hiveCtx.registerFunction("strLenPython", lambda x: len(x), IntegerType())
+    lengthSchemaRDD = hiveCtx.hql("SELECT strLenPython('text') FROM tweets LIMIT 10")
     print lengthSchemaRDD.collect()
     sc.stop()
