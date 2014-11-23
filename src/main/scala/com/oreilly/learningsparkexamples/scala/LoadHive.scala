0f108fb9e47a3e6103bf328b56c4dadf45379fd0
Taka Shinagawa <taka.epsilon@gmail.com>
2014-11-23 14:13:31 -0800

    Removed unnecessary package names for SQLContext() and HiveContext()

--- src/main/scala/com/oreilly/learningsparkexamples/scala/LoadHive.scala
+++ src/main/scala/com/oreilly/learningsparkexamples/scala/LoadHive.scala
@@ -16,7 +16,7 @@ object LoadHive {
     val master = args(0)
     val tableName = args(1)
     val sc = new SparkContext(master, "LoadHive", System.getenv("SPARK_HOME"))
-    val hiveCtx = new org.apache.spark.sql.hive.HiveContext(sc)
+    val hiveCtx = new HiveContext(sc)
     val input = hiveCtx.sql("FROM src SELECT key, value")
     val data = input.map(_.getInt(0))
     println(data.collect().toList)
